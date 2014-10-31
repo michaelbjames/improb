140e8ec7f8295a74cc0d2da5e1914c41ccfbe807
Michael James <michael@michaelbjames.com>
2014-10-31 15:43:16 -0400

    untested program parser

--- parser.hs
+++ parser.hs
@@ -2,19 +2,70 @@ module Parser where
 
 import Text.Parsec
 import Text.Parsec.String
-import Text.Parsec.Token hiding (natural)
+import Text.Parsec.Token hiding (natural, identifier)
 --import Text.ParserCombinators.Parsec.Token hiding (natural)
 
 import AST
 
+programParser :: Parser Program
+programParser = do
+    aliases <- many1 (try (withSpacing aliasParser))
+    many newline
+    tempo <- withSpacing tempoParser
+    many newline
+    voices <- many1 (withSpacing voiceParser)
+    return (Program aliases tempo voices)
+    where
+        withSpacing p = do
+            parsed <- p
+            many newline
+            return parsed
 
---main = do
---    let input = "A"
+        tempoParser = do
+            string "tempo"
+            spaces
+            char ':'
+            spaces
+            natural
 
---parseKey :: String -> Either ParseError Key
+aliasParser :: Parser Alias
+aliasParser = do
+    name <- many1 alphaNum
+    spaces
+    string ":="
+    spaces
+    pattern <- musicPatternParser
+    return (Alias {identifier = name, pattern = pattern})
+
+voiceParser :: Parser Voice
+voiceParser = do
+    instrument <- between (char ':') (char ':') (many1 alphaNum)
+    transitions <- sepBy1 transitionParser newline
+    return (Voice instrument transitions)
+
+transitionParser :: Parser Transition
+transitionParser =
+        try intro
+    <|> try transition
+    where
+        intro :: Parser Transition
+        intro = do
+            string "=>"
+            spaces
+            mp <- musicPatternParser
+            return (Intro mp)
+
+        transition :: Parser Transition
+        transition = do
+            start <- musicPatternParser
+            spaces
+            string "=>"
+            spaces
+            end <- musicPatternParser
+            return (Transition start end)
 
 musicPatternParser :: Parser MusicPattern
-musicPatternParser = chainr1 singleLiteral continuation
+musicPatternParser = chainr1 singleLiteral (try continuation)
 -- thanks http://stuckinaninfiniteloop.blogspot.com/2011/10/left-recursion-in-parsec.html
     where
         singleLiteral :: Parser MusicPattern
@@ -78,7 +129,7 @@ toneParser :: Parser Tone
 toneParser = do
     key <- keyParser
     octave <- natural
-    return (Tone {octave=octave, key=key})
+    return (Tone {octave = octave, key = key})
 
 keyParser :: Parser Key
 keyParser =
